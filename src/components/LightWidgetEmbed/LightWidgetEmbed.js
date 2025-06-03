a56fbba2f76d349a27522b7fcbb8212aa33e8b58
Leandro da Silva Rodrigues <163856588+leandro-srod@users.noreply.github.com>
2025-06-03 14:23:18 -0300

    LightWidget PAGO Install

--- src/components/LightWidgetEmbed/LightWidgetEmbed.js
+++ src/components/LightWidgetEmbed/LightWidgetEmbed.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 function LightWidgetComponent() {
-  const widgetCode = `<iframe src="//lightwidget.com/widgets/bd562e7babd7534b9b1e67b55ecfafe5.html" scrolling="no" allowtransparency="true" 
-class="lightwidget-widget" style="width:100%; border:0; overflow:auto;"></iframe>
+  const widgetCode = `<iframe src="https://cdn.lightwidget.com/widgets/bd562e7babd7534b9b1e67b55ecfafe5.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:auto;"></iframe>
   `;
-
   return (
       <div dangerouslySetInnerHTML={{ __html: widgetCode }}></div>
   );
